be66300c21b87bbe114b3fffa40433f9d41bd32f
Tony Taylor <tony@tonetaylor.com>
2024-03-17 16:35:11 -0500

    Adds task #4.

--- 00-investigating-texts-and-calls/Task4.py
+++ 00-investigating-texts-and-calls/Task4.py
@@ -24,4 +24,61 @@ Print a message:
 <list of numbers>
 The list of numbers should be print out one per line in lexicographic order with no duplicates.
 """
+COLUMN_CALL_ORIGINATING_NUMBER = 0
+COLUMN_CALL_RECEIVING_NUMBER = 1
+COLUMN_CALL_TIMESTAMP = 2
+COLUMN_CALL_DURATION = 3
+COLUMN_TEXT_ORIGINATING_NUMBER = 0
+COLUMN_TEXT_RECEIVING_NUMBER = 1
+COLUMN_TEXT_TIMESTAMP = 2
 
+
+def get_originating_calls(data):
+    return [x[COLUMN_CALL_ORIGINATING_NUMBER] for x in data]
+
+
+def filter_originating_calls_not_in_data_column(originating_calls, data, column_index):
+    output = []
+
+    for call in originating_calls:
+        for x in data:
+            if x[column_index] == call:
+                break
+        else:
+            output.append(call)
+
+    return output
+
+
+def find_possible_telemarketers():
+    originating_calls = get_originating_calls(calls)
+    output = []
+
+    filtered_receiving_calls = filter_originating_calls_not_in_data_column(
+        originating_calls, calls, COLUMN_CALL_RECEIVING_NUMBER
+    )
+    filtered_originating_texts = filter_originating_calls_not_in_data_column(
+        originating_calls, texts, COLUMN_CALL_ORIGINATING_NUMBER
+    )
+    filtered_receiving_texts = filter_originating_calls_not_in_data_column(
+        originating_calls, texts, COLUMN_TEXT_RECEIVING_NUMBER
+    )
+    all_filters = filtered_receiving_calls + filtered_originating_texts + filtered_receiving_texts
+
+    for c in all_filters:
+        for entry in output:
+            if entry == c:
+                break
+        else:
+            output.append(c)
+
+    return sorted(output)
+
+
+def get_telemarketer_report(possible_telemarketers):
+    return "These numbers could be telemarketers:\n{0}".format(
+        "\n".join(possible_telemarketers)
+    )
+
+
+print(get_telemarketer_report(find_possible_telemarketers()))
