f353539febc3eadae890d155f6e36fe8a8bc2b70
Tony Taylor <tone.taylor@gmail.com>
2024-03-25 20:56:34 -0500

    Corrects and verifies Task #2

--- 00-investigating-texts-and-calls/Task2.py
+++ 00-investigating-texts-and-calls/Task2.py
@@ -27,11 +27,14 @@ def is_match_for(date_string, month, year):
     return True if dt.year == year and dt.month == month else False
 
 
-matches = [x for x in calls if is_match_for(x[2], 9, 2016)]
-sorted(matches, key=lambda match: match[3])
-longest_call = matches[len(matches) - 1]
+def run():
+    matches = [x for x in calls if is_match_for(x[2], 9, 2016)]
+    sorted_matches = sorted(matches, key=lambda match: int(match[3]), reverse=True)
+    longest_call = sorted_matches[0]
 
+    print("{0} spent the longest time, {1} seconds, on the phone during September 2016.".format(
+        longest_call[0], longest_call[len(longest_call) - 1]
+    ))
 
-print("{0} spent the longest time, {1} seconds, on the phone during September 2016.".format(
-    longest_call[0], longest_call[len(longest_call) - 1]
-))
+if __name__ == '__main__':
+    run()
